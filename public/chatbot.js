c76a710e82babbbee07b4f0633cd89396da7a7be
Elmira <salary.elmira@gmail.com>
2026-05-12 17:08:41 -0500

    Fix chat endpoint

--- public/chatbot.js
+++ public/chatbot.js
@@ -7,7 +7,7 @@ async function sendMessage() {
   addMessage(message, "user-message");
   input.value = "";
 
-  const response = await fetch("http://localhost:3000/chat", {
+  const response = await fetch("/chat", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
